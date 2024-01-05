da7e9fd5a55acd4f7ff9d559a8f6385f2ab82e47
MrDinsdale <joedinsdale@pm.me>
2024-01-05 10:37:00 +0000

    remove duplicated brackets

--- keyboards/avalanche/v4/keymaps/cornish/keymap.c
+++ keyboards/avalanche/v4/keymaps/cornish/keymap.c
@@ -43,24 +43,24 @@ enum layer {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALPHA] = LAYOUT(
-             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-             GUI_TAB, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                        KC_J,       KC_L,      KC_U,       KC_Y,       KC_SCLN,    GUI_ESC,
-    XXXXXXX, C_CAPS,  HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,                                        KC_M,       HOME_N,    HOME_E,     HOME_I,     HOME_O,     C_SLASH, XXXXXXX,
-             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_K,       KC_H,      KC_GRV,     KC_COMM,    KC_DOT,     A_QUOTE,
+             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,
+             GUI_TAB, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                        KC_J,       KC_L,      KC_U,       KC_Y,    KC_SCLN, GUI_ESC,
+    XXXXXXX, C_CAPS,  HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,                                        KC_M,       HOME_N,    HOME_E,     HOME_I,  HOME_O,  C_SLASH, XXXXXXX,
+             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_K,       KC_H,      KC_GRV,     KC_COMM, KC_DOT,  A_QUOTE,
                                         KC_MUTE, SGUI_1,  KC_SPC,  LSY_ENT, XXXXXXX, XXXXXXX, KC_BSPC, LFN_DEL,    LCAG_1,    KC_HOME
   ),
   [_SYMBOLS] = LAYOUT(
-             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-             _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,       KC_7,      KC_8,       KC_9,       KC_0,       _______,
-    XXXXXXX, _______, _______, _______, _______, _______, XXXXXXX,                                     KC_MINS,    KC_EQL,    KC_BSLS,    KC_LBRC,    KC_RBRC,    _______, XXXXXXX,
-             _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_MINS), S(KC_EQL), S(KC_BSLS), S(KC_LBRC), S(KC_RBRC), _______,
+             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,
+             _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,       KC_7,      KC_8,       KC_9,    KC_0,    _______,
+    XXXXXXX, _______, _______, _______, _______, _______, XXXXXXX,                                     KC_MINS,    KC_EQL,    KC_BSLS,    _______, _______, _______, XXXXXXX,
+             _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_MINS), S(KC_EQL), S(KC_BSLS), _______, _______, _______,
                                         _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______,    _______,   _______
   ),
   [_NAVIGATION] = LAYOUT(
-             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-             _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                       KC_F6,      KC_F7,     KC_F8,      KC_F9,      KC_F10,     _______,
-    XXXXXXX, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,                                     XXXXXXX,    _______,   _______,    _______,    _______,    _______, XXXXXXX,
-             _______, RGB_MOD, RGB_HUI, RGB_M_P, RGB_M_SW,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,
+             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,
+             _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                       KC_F6,      KC_F7,     KC_F8,      KC_F9,   KC_F10,  _______,
+    XXXXXXX, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,                                     XXXXXXX,    _______,   _______,    _______, _______, _______, XXXXXXX,
+             _______, RGB_MOD, RGB_HUI, RGB_M_P, RGB_M_SW,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX, _______,
                                         RGB_TOG, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______,    _______,   _______
   )
 };
